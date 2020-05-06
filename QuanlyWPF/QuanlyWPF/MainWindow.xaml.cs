3003564bf86f8b2e805091c65f416f74cf2340e4
quang son <quangsonk2e@gmail.com>
2020-05-06 14:20:19 +0700

    Init

---
+++ QuanlyWPF/QuanlyWPF/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanlyWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PopupBox_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PopupBox_Opened(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PopupBox_Closed(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ QuanlyWPF/QuanlyWPF/ViewModel/test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace QuanlyWPF.ViewModel
+{
+    public class test
+    {
+        
+        public test()
+        {
+            mesText = new RelayCommand<object>(c =>true, c => {                
+                MessageBox.Show("Bạn đã nhập");
+            });
+        }
+        public ICommand mesText { get; set; }
+    }
+}
